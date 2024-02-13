0649d9dae6d1cb43717881ac62f44fea634306e7
MuriithiMark <markmwangi14317@gmail.com>
2024-02-13 11:14:25 +0300

    initial commit for redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-import Button_component, { Button_class_component } from './components/Button'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
    <>
-   <Button_class_component btnName="Class Btn" btnColor="red" />
-   <Button_component btnName="Func Btn" btnColor="blue"  />
+ 
    </>
   )
 }
