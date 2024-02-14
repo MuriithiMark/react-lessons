d1dc56ef76da128cd5157ff900d927472b2f2c35
MuriithiMark <markmwangi14317@gmail.com>
2024-02-14 10:14:08 +0300

    clean project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import Button_component, { Button_class_component } from './components/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
-   <Button_class_component btnName="Class Btn" btnColor="red" />
-   <Button_component btnName="Func Btn" btnColor="blue"  />
+ 
    </>
   )
 }
