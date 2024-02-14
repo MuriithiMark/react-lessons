304a1a86feb7ac1d0ab02e1cafc0c1c25073f6a0
MuriithiMark <markmwangi14317@gmail.com>
2024-02-14 09:57:06 +0300

    redux counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css';
+import "./App.css";
+import { decrement, increment } from "./lib/features/counter/counterSlice";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
-   <>
- 
-   </>
-  )
+    <>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <p>{count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <div className="add-by-payload">
+        {/* <input type="number" name="payload" id="add_to_count" value={}/> */}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
